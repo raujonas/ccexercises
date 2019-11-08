939649a4dd9c74f08cfba489e5695957a023075f
Jonas Rau <rau.jonas@web.de>
2019-11-08 21:19:31 +0100

    getting all items from dynamo db working

--- e4t3/src/main/java/de/unistuttgart/iaas/cc/exfour/IndexController.java
+++ e4t3/src/main/java/de/unistuttgart/iaas/cc/exfour/IndexController.java
@@ -1,11 +1,20 @@
 package de.unistuttgart.iaas.cc.exfour;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.UUID;
 
 @Controller
 public class IndexController {
@@ -19,7 +28,34 @@ public class IndexController {
 
 	    // TODO: Task 4 - Request database for content of cart
 
-		model.addAttribute("cart", "this is the content");
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+				.build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+
+		Table table = dynamoDB.getTable("ShoppingCart");
+
+		ScanSpec scanSpec = new ScanSpec();
+
+		try {
+			ItemCollection<ScanOutcome> items = table.scan(scanSpec);
+
+			String content = "";
+
+			Iterator<Item> iter = items.iterator();
+			while (iter.hasNext()) {
+				Item item = iter.next();
+				System.out.println(item.toString());
+				content = content + " " + item.getJSON("item").replace("\"", "") + ", ";
+			}
+
+			model.addAttribute("cart", content);
+		}
+		catch (Exception e) {
+			System.err.println("Unable to scan the table:");
+			System.err.println(e.getMessage());
+		}
+
 		return "index";
 	}
 }
